5ac97925aab5335381551419b6874d6a352fdfa6
SYU2017 <shuzhenyu888@gmail.com>
2017-10-07 08:41:13 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,95 @@
 import os
 import csv
+import datetime
 
-newbudgetdata2 = os.path.join('..','Resources', "budget_data2.csv")
-    
-    total_months = 0
-    total_revenue = 0
-    avg_rev_change = 0
-    current_month_rev = 0
-    period_month_rev = 0
-    
-    change_in_rev = []
+employee_csv = os.path.join("..", "Resources", "employee_data2.csv")
 
-with open(newbudgetdata2, "r" ) as csvfile:
+Emp_ID = []
+First_Name= []
+Last_Name = []
+DOB = []
+SSN = []
+State = []
+us_state_abbrev = {
+          'Alabama': 'AL',
+          'Alaska': 'AK',
+          'Arizona': 'AZ',
+          'Arkansas': 'AR',
+          'California': 'CA',
+          'Colorado': 'CO',
+          'Connecticut': 'CT',
+          'Delaware': 'DE',
+          'Florida': 'FL',
+          'Georgia': 'GA',
+          'Hawaii': 'HI',
+          'Idaho': 'ID',
+          'Illinois': 'IL',
+          'Indiana': 'IN',
+          'Iowa': 'IA',
+          'Kansas': 'KS',
+          'Kentucky': 'KY',
+          'Louisiana': 'LA',
+          'Maine': 'ME',
+          'Maryland': 'MD',
+          'Massachusetts': 'MA',
+          'Michigan': 'MI',
+          'Minnesota': 'MN',
+          'Mississippi': 'MS',
+          'Missouri': 'MO',
+          'Montana': 'MT',
+          'Nebraska': 'NE',
+          'Nevada': 'NV',
+          'New Hampshire': 'NH',
+          'New Jersey': 'NJ',
+          'New Mexico': 'NM',
+          'New York': 'NY',
+          'North Carolina': 'NC',
+          'North Dakota': 'ND',
+          'Ohio': 'OH',
+          'Oklahoma': 'OK',
+          'Oregon': 'OR',
+          'Pennsylvania': 'PA',
+          'Rhode Island': 'RI',
+          'South Carolina': 'SC',
+          'South Dakota': 'SD',
+          'Tennessee': 'TN',
+          'Texas': 'TX',
+          'Utah': 'UT',
+          'Vermont': 'VT',
+          'Virginia': 'VA',
+          'Washington': 'WA',
+          'West Virginia': 'WV',
+          'Wisconsin': 'WI',
+          'Wyoming': 'WY' }
+
+
+with open(employee_csv, newline="") as csvfile:
     csvreader= csv.reader(csvfile, delimiter=",")
-    headerline = csvfile.next()
     
-for row in csvreader:
+    for row in csvreader:
+        Emp_ID.append(row[0])
+        first = row[1].split( )[0]
+        last = row[1].split( )[1]
+        First_Name.append(first)
+        Last_Name.append(last)
+      
+        newdate=row[2].datetime.strptime("2017-1-25", '%Y-%m-%d').strftime('%-m/%d/%y') 
+        DOB.append(newdate)
+        newSSN="*" * (len(raw[3]) - 4) + raw[3][-4:]
+        SSN.append(newSSN)
+        
+        newstate = us_state_abbrev.get(row[4].upper(), "")
 
-    total_months = sum(map(int, row))-1
-    #print( "Total Months : " + str(total_months))
-    total_revenue += int(row[1])
-    #print( "Total Revenue : " +"$"+ str(total_revenue)
-    if total_months > 1:
-          change = current_month_rev - period_month_rev
-          change_in_rev.append(change)
-          
-total_change = 0
-for i in range(len(change_in_rev)):
-    total_change = total_change + change_in_rev[i]
+        State.append(newstate)
 
-    avg_rev_change = total_change / len(change_in_rev)
- 
-    print("Total Months: " + str(total_months))
-    print( "Total Revenue : " +"$"+ str(total_revenue)
-    print("Average Revenue Change:" +"$"+  str(round(avg_rev_change,2)))
-output_file = os.path.join('../Resources','Pybank' + ".txt") 
-with open(output_file, 'w') as textfile:
-    textfile.write("Total Months: " + str(total_months)+ "\n")
-    textfile.write("Total Revenue: "+" $" + str(total_revenue) + "\n")
-    textfile.write("Average Revenue Change: "+" $" + str(round(avg_rev_change, 2))+ "\n")
+cleaned_csv = zip(Emp_ID, First_Name, Last_Name, DOB, SSN, State )
+print(cleaned_csv)
+# Set variable for output file
+output_file = os.path.join("employee_final.csv")
+#  Open the output file
+with open(output_file, "w", newline="") as datafile:
+    writer = csv.writer(datafile)
+    # Write the header row
+    writer.writerow(["Emp_ID", "First_Name", "Last_Name", "DOB", "SSN", "State"])
+    # Write in zipped rows
+    writer.writerows(cleaned_csv)
